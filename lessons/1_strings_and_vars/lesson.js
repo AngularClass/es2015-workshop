9842e33905aff8cd61ed30e27ff9f94392c17d1f
Hendrixer <willscottmoss@gmail.com>
2015-12-05 23:10:59 -0800

    chore

--- lessons/1_strings_and_vars/lesson.js
+++ lessons/1_strings_and_vars/lesson.js
@@ -41,10 +41,33 @@ logger('debbuger', 'data value: ' + data.value);
 var message = 'Hey there ${name}, do you like ${food} ?';
 var data = {name: 'Rauria', food: 'butter cake'};
 
-for (key in data) {
+for (var key in data) {
   var interopKey = '${'+key+'}';
   if (message.includes(interopKey)) {
     message = message.replace(interopKey, data[key]);
   }
 }
 console.log(message);
+/*******************************************/
+
+
+function FancyArray() {
+  Array.call(this, arguments);
+  this.count = arguments.length;
+  // static method
+  this.toString = function() {
+    return '[FancyArray array]';
+  };
+}
+
+FancyArray.prototype = Object.create(Array.prototype);
+
+FancyArray.prototype.last = function() {
+  return this[this.length - 1];
+};
+
+var array = new FancyArray();
+array.push(1, 2, 3, 4, 5, 6, 7);
+
+console.log(array.last());
+console.log(array.toString());
